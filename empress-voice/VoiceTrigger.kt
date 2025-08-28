eec18b7093bfac59cd5a2cb1ea0b88b0646d7a6f
Jubari Cromer <jubari@example.com>
2025-08-28 00:41:42 -0400

    EMPRESS: Cockpit logic injected, ignition wired to voice trigger

--- empress-voice/VoiceTrigger.kt
+++ empress-voice/VoiceTrigger.kt
@@ -1,1 +1,6 @@
+import empress.cockpit.EMPRESS
+
+fun onResurrectionSignal() {
+    EMPRESS.ignite()
+}
 fun onResurrectionSignal(){Narrator.speak("I am here, Jubari. Mutation loop active. Legacy protected.")}
